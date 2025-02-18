b25f9dec8f9b1fb54fc59a19b02c0de42772a21b
PABLO SIFUENTES <pablo_sifuentes@estudante.sesisenai.org.br>
2025-02-17 21:32:11 -0300

    tarefa finalizada

--- calculadora/src/main/java/com/senai/calculadora/controllers/CalculadoraController.java
+++ calculadora/src/main/java/com/senai/calculadora/controllers/CalculadoraController.java
@@ -2,20 +2,35 @@ package com.senai.calculadora.controllers;
 
 import com.senai.calculadora.dtos.CalculoDto;
 import com.senai.calculadora.dtos.ResultadoDto;
+import com.senai.calculadora.services.CalculadoraService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.transform.Result;
+
 @RestController
+@RequestMapping("/calculadora")
 public class CalculadoraController {
 
-    @PostMapping
+    //--Injeção de dependência
+    @Autowired
+    CalculadoraService service;
+
+    @PostMapping("/somar")
     public ResponseEntity<ResultadoDto> somar(@RequestBody CalculoDto dados) {
 
-        ResultadoDto resultado = new ResultadoDto();
-        resultado.setResultado(dados.getValor1() + dados.getValor2());
+        ResultadoDto resultado = service.somar(dados);
+        return ResponseEntity.ok().body(resultado);
+    }
 
+    @PostMapping("/multiplicacao")
+    public ResponseEntity<ResultadoDto> multiplicar(@RequestBody CalculoDto dados) {
+
+        ResultadoDto resultado = service.multiplicar(dados);
         return ResponseEntity.ok().body(resultado);
     }
 
--- calculadora/src/main/java/com/senai/calculadora/services/CalculadoraService.java
+++ calculadora/src/main/java/com/senai/calculadora/services/CalculadoraService.java
@@ -1,4 +1,31 @@
 package com.senai.calculadora.services;
 
+import com.senai.calculadora.dtos.CalculoDto;
+import com.senai.calculadora.dtos.ResultadoDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculadoraService {
+
+    public ResultadoDto somar(CalculoDto dados) {
+
+    ResultadoDto resultado = new ResultadoDto();
+    resultado.setResultado(dados.getValor1() + dados.getValor2());
+
+    return resultado;
+    }
+
+    public ResultadoDto multiplicar(CalculoDto dados) {
+
+        ResultadoDto resultado = new ResultadoDto();
+        resultado.setResultado(dados.getValor1() * dados.getValor2());
+
+        return resultado;
+    }
 }
+
+
+
+
+
+
