b83deaa532a791cc38c889db9d308a782a6aa3dd
Pablo Sifuentes <sifuentespablo22@gmail.com>
2025-06-01 20:54:47 -0300

    falta continuar, model criado

---
+++ Gerenciamento_EPI_SA/src/main/java/com/senai/Gerenciamento_EPI_SA/Model/ColaboradoresDto.java
@@ -0,0 +1,77 @@
+package com.senai.Gerenciamento_EPI_SA.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "COLABORADOR")
+public class ColaboradoresDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "nome")
+    private String nome;
+
+    @Column(nullable = false, unique = true , name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "funcao")
+    private String funcao;
+
+    @Column(nullable = false, name = "data_nascimento")
+    private LocalDate nascimento;
+
+    public ColaboradoresDto() {
+    }
+
+    public ColaboradoresDto(Long id, String nome, String email, String funcao, LocalDate nascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.funcao = funcao;
+        this.nascimento = nascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+}
--- Gerenciamento_EPI_SA/src/main/java/com/senai/Gerenciamento_EPI_SA/Model/UsuarioModel.java
+++ Gerenciamento_EPI_SA/src/main/java/com/senai/Gerenciamento_EPI_SA/Model/UsuarioModel.java
@@ -1,16 +1,10 @@
-package com.senai.Gerenciamento_EPI_SA;
+package com.senai.Gerenciamento_EPI_SA.Model;
 
 import jakarta.persistence.*;
-
-import java.util.regex.Pattern;
 
 @Entity
 @Table(name = "USUARIO")
 public class UsuarioModel {
-
-    private static final int TAMANHO_MINIMO_SENHA = 5;
-
-    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,9 +42,6 @@ public class UsuarioModel {
     }
 
     public void setNome(String nome) {
-        if (nome == null || nome.trim().isEmpty()){
-            throw new IllegalArgumentException("Campo não pode ser em branco");
-        }
         this.nome = nome;
     }
 
@@ -59,9 +50,6 @@ public class UsuarioModel {
     }
 
     public void setEmail(String email) {
-        if (email == null || email.trim().isEmpty()){
-            throw new IllegalArgumentException("Campo não pode ser em branco");
-        }
         this.email = email;
     }
 
@@ -70,20 +58,7 @@ public class UsuarioModel {
     }
 
     public void setSenha(String senha) {
-        if (!senhaValida(senha)) {
-            throw new IllegalArgumentException(
-                    "Senha deve ter no mínimo " + TAMANHO_MINIMO_SENHA + " caracteres e não pode ser em branco");
-        }
         this.senha = senha;
     }
-
-    public static boolean senhaValida(String senha){
-        return senha != null &&
-                !senha.trim().isEmpty() &&
-                senha.length() >= TAMANHO_MINIMO_SENHA;
-    }
-
-    public static boolean EmailValido(String email) {
-        return email != null && EMAIL_PATTERN.matcher(email).matches();
-    }
 }
+
