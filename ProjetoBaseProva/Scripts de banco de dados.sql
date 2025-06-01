916209cde92a94d2e870c6d0c1d10f5df2e3ccca
Pablo Sifuentes <sifuentespablo22@gmail.com>
2025-06-01 20:58:16 -0300

    prova atualizada

--- ProjetoBaseProva/Scripts de banco de dados.sql
+++ ProjetoBaseProva/Scripts de banco de dados.sql
@@ -1,2 +1,7 @@
-SELECT 'USUARIOS cadastrados:' AS Mensagem;
-SELECT * FROM USUARIO;
+/*-- Inserção de 5 usuários*/
+INSERT INTO USUARIO (ID, EMAIL, SENHA) VALUES
+(1, 'joao.silva@email.com', 'senha123'),
+(2, 'maria.oliveira@email.com', 'm@ri@456'),
+(3, 'carlos.pereira@email.com', 'c@rl0s789'),
+(4, 'ana.santos@email.com', 'an@2023'),
+(5, 'pedro.costa@email.com', 'p3dr0S3nha');
